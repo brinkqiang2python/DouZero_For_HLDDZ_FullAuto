cce586f3e5686146cb9e54134a1782604bc82673
brinkqiang <brink.qiang@gmail.com>
2022-04-19 02:40:11 +0800

    add

--- debug_screenshot.py
+++ debug_screenshot.py
@@ -1,9 +1,11 @@
+
 import GameHelper as gh
 from GameHelper import GameHelper
 
 GameHelper = GameHelper()
-GameHelper.ScreenZoomRate = 1.25
+GameHelper.ScreenZoomRate = 1.00
 img, _ = GameHelper.Screenshot()
-img = gh.DrawRectWithText(img, (313, 747, 1144, 200), "test")
+
+img = gh.DrawRectWithText(img, (1443, 1, 913, 73), "test")
 img.save("test.png")
 gh.ShowImg(img)
